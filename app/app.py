88687b38da8ef8bbc6d7bfbf55e8c8f67ec2849a
wendwin <wendwin12@gmail.com>
2024-09-10 04:10:11 +0700

    membuat tampilan materi

--- app/app.py
+++ app/app.py
@@ -4,19 +4,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    css = 'beranda.css'
+    css = 'beranda.css' 
     return render_template('home.html', css=css)
 
 @app.route('/materi')
 def materi():
-    return render_template('materi.html')
+    css = 'beranda.css'
+    return render_template('materi/materi.html', css=css)
+
+@app.route('/materi/emosi-dasar')
+def materi_emosi_dasar():
+    return render_template('materiemosidasar.html')
+
+@app.route('/materi/emosi-gabungan')
+def materi_emosi_gabungan():
+    return render_template('materiemosigabungan.html')
 
 @app.route('/latihan')
 def latihan():
     return render_template('latihan.html')
 
 @app.route('/laporan/rombongan')
-def laporanrombongan():
+def laporan_rombongan():
     return render_template('laporanrombongan.html')
 
 
